15b81dd7feedd929749c79d94604e202f350602d
Bhargav <bhargavbhimani55@gmail.com>
2023-10-25 12:32:04 +0530

    design issue fix

--- src/components/input-field.vue
+++ src/components/input-field.vue
@@ -1,37 +1,39 @@
 <template>
-    <div class="flex flex-col justify-center w-[80vw] items-end rounded-md h-[25vh] md:h-[36vh]">
-        <input type="text" @input="onKeyboardFunc" v-model="inputString" inputmode="numeric" @keydown.enter="calculate"
-            id="inputField" ref="myInput"
-            class="bg-slate-950 w-full h-20 mx-auto outline-none px-5 text-custom-cursor cursor-pointer text-right text-2xl bottom-0 focus:cursor-text">
-        <p class="text-slate-300 text-3xl px-5 ">{{ this.result }}</p>
-    </div>
-    <div class="grid grid-cols-4 gap-5">
-        <OperatorButton @click="clearInput">C</OperatorButton>
-        <OperatorButton @click="handleFunction('%')" @input="handleFunction">%</OperatorButton>
-        <OperatorButton @click="clearLast">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" style="background-color:inherit;"
-                viewBox="0 0 24 24">
-                <path fill="currentColor"
-                    d="M19 15.59L17.59 17L14 13.41L10.41 17L9 15.59L12.59 12L9 8.41L10.41 7L14 10.59L17.59 7L19 8.41L15.41 12L19 15.59M22 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H7c-.69 0-1.23-.36-1.59-.89L0 12l5.41-8.12C5.77 3.35 6.31 3 7 3h15m0 2H7l-4.72 7L7 19h15V5Z" />
-            </svg>
-        </OperatorButton>
-        <OperatorButton @click="handleFunction('/')" @input="checkInput">/</OperatorButton>
-        <TheButton @click="handleFunction(7)">7</TheButton>
-        <TheButton @click="handleFunction(8)">8</TheButton>
-        <TheButton @click="handleFunction(9)">9</TheButton>
-        <OperatorButton @click="handleFunction('*')">X</OperatorButton>
-        <TheButton @click="handleFunction(4)">4</TheButton>
-        <TheButton @click="handleFunction(5)">5</TheButton>
-        <TheButton @click="handleFunction(6)">6</TheButton>
-        <OperatorButton @click="handleFunction('-')" class="text-3xl">-</OperatorButton>
-        <TheButton @click="handleFunction(1)">1</TheButton>
-        <TheButton @click="handleFunction(2)">2</TheButton>
-        <TheButton @click="handleFunction(3)">3</TheButton>
-        <OperatorButton @click="handleFunction('+')" class="text-3xl">+</OperatorButton>
-        <TheButton @click="handleFunction('00')">00</TheButton>
-        <TheButton @click="handleFunction(0)">0</TheButton>
-        <TheButton @click="handleFunction('.')">.</TheButton>
-        <EqlsBtn @click="calculate">=</EqlsBtn>
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex flex-col justify-center w-full items-end rounded-md h-[25vh] md:h-[36vh]">
+            <input type="text" @input="onKeyboardFunc" v-model="inputString" inputmode="numeric" @keydown.enter="calculate"
+                id="inputField" ref="myInput"
+                class="bg-slate-950 w-full h-20 mx-auto outline-none px-5 text-custom-cursor cursor-pointer text-right text-2xl bottom-0 focus:cursor-text">
+            <p class="text-slate-300 text-3xl px-5 ">{{ this.result }}</p>
+        </div>
+        <div class="grid grid-cols-4 gap-5">
+            <OperatorButton @click="clearInput">C</OperatorButton>
+            <OperatorButton @click="handleFunction('%')" @input="handleFunction">%</OperatorButton>
+            <OperatorButton @click="clearLast">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" style="background-color:inherit;"
+                    viewBox="0 0 24 24">
+                    <path fill="currentColor"
+                        d="M19 15.59L17.59 17L14 13.41L10.41 17L9 15.59L12.59 12L9 8.41L10.41 7L14 10.59L17.59 7L19 8.41L15.41 12L19 15.59M22 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H7c-.69 0-1.23-.36-1.59-.89L0 12l5.41-8.12C5.77 3.35 6.31 3 7 3h15m0 2H7l-4.72 7L7 19h15V5Z" />
+                </svg>
+            </OperatorButton>
+            <OperatorButton @click="handleFunction('/')" @input="checkInput">/</OperatorButton>
+            <TheButton @click="handleFunction(7)">7</TheButton>
+            <TheButton @click="handleFunction(8)">8</TheButton>
+            <TheButton @click="handleFunction(9)">9</TheButton>
+            <OperatorButton @click="handleFunction('*')">X</OperatorButton>
+            <TheButton @click="handleFunction(4)">4</TheButton>
+            <TheButton @click="handleFunction(5)">5</TheButton>
+            <TheButton @click="handleFunction(6)">6</TheButton>
+            <OperatorButton @click="handleFunction('-')" class="text-3xl">-</OperatorButton>
+            <TheButton @click="handleFunction(1)">1</TheButton>
+            <TheButton @click="handleFunction(2)">2</TheButton>
+            <TheButton @click="handleFunction(3)">3</TheButton>
+            <OperatorButton @click="handleFunction('+')" class="text-3xl">+</OperatorButton>
+            <TheButton @click="handleFunction('00')">00</TheButton>
+            <TheButton @click="handleFunction(0)">0</TheButton>
+            <TheButton @click="handleFunction('.')">.</TheButton>
+            <EqlsBtn @click="calculate">=</EqlsBtn>
+        </div>
     </div>
 </template>
 
